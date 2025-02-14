444eb77b2b8d79c7ef72551abb40e80f27ffea3c
JornToomingas <jmarkus@hkhk.edu.ee>
2025-02-14 14:02:23 +0200

    Add files via upload

---
+++ Ülesanne09.ps1
@@ -0,0 +1,38 @@
+﻿$kasutajad = Import-Csv -Path "F:\Skriptid\users.csv"
+
+$uuskasutajad = @()
+
+function Genereeri-Parool {
+    param (
+        [string]$perekonnanimi
+    )
+    $perekonnaosa = $perekonnanimi.Substring(0, 3).ToLower()
+    $numbriteosa = -join ((0..9) | Get-Random -Count 2)
+    return $perekonnaosa + $numbriteosa
+}
+
+foreach ($kasutaja in $kasutajad) {
+    $eesnimi = $kasutaja.eesnimi
+    $perekonnanimi = $kasutaja.perenimi
+
+    $nimi = "$eesnimi $perekonnanimi"
+    $kasutajanimi = ($eesnimi.Substring(0, 1) + $perekonnanimi).ToLower()
+    $email = "$eesnimi.$perekonnanimi@hkhk.edu.ee"
+    $parool = Genereeri-Parool -perekonnanimi $perekonnanimi
+
+
+
+    $uusKasutaja = [PSCustomObject]@{
+        Nimi         = $nimi
+        Kasutajanimi = $kasutajanimi
+        Email        = $email
+        Parool       = $parool
+    }
+    $uusKasutajad += $uusKasutaja
+}
+
+
+$uusKasutajad | Export-Csv -Path "F:\Skriptid\uued_kasutajad.csv"
+
+Write-Host "Uus csv fail on tehtud: uued_kasutajad.csv"
+
---
+++ Ülesanne10.ps1
@@ -0,0 +1,13 @@
+﻿[xml]$customers = Get-Content -Path "F:/Skriptid/customers.xml"
+
+
+
+$polandCustomers = $customers.customers.customer | Where-Object { $_.country -eq "Poland" }
+
+foreach ($customer in $polandCustomers) {
+    if ($customer.name) {
+        Write-Output $customer.name
+    } else {
+        Write-Output "Name is missing"
+    }
+}
