0fc3153a5a5d0e58ac0cbde0d46e6456975a218b
JornToomingas <jmarkus@hkhk.edu.ee>
2025-02-20 11:24:51 +0200

    Add files via upload

---
+++ Inaktiivsed_kasutajad.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module ActiveDirectory
+
+$inactiveOU = "OU=INAKTIIVSED KASUTAJAD,DC=TOOMINGAS,DC=LOCAL"
+$Aasta = (Get-Date).AddYears(-1)
+
+$users = Get-ADUser -Filter * -Properties LastLogonDate
+
+foreach ($user in $users) {
+    if ($user.LastLogonDate -eq $null -or $user.LastLogonDate -lt $oneYearAgo) {
+        Move-ADObject -Identity $user.DistinguishedName -TargetPath $inactiveOU
+        Disable-ADAccount -Identity $user.SamAccountName
+        Write-Output "Kasutaja $($user.SamAccountName) on liigutatud ja deaktiveeritud."
+    }
+}
